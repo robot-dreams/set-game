a645774713ef719abc087f9f6a130b341faea1da
Elliott Jin <elliott.jin@gmail.com>
2023-12-21 11:04:32 -0800

    Improve rendering
    "Please update the CSS and HTML to render card properties."
    "What corresponding changes are needed in the JavaScript code?"

--- script.js
+++ script.js
@@ -2,36 +2,51 @@ document.getElementById('newGame').addEventListener('click', newGame);
 let selectedCards = [];
 
 function newGame() {
-    // Reset game board and status
     document.getElementById('gameBoard').innerHTML = '';
     document.getElementById('gameStatus').innerText = '';
+    selectedCards = [];
 
-    // Generate and display cards (simplified)
+    // Generate and display cards
     for (let i = 0; i < 12; i++) {
-        let card = document.createElement('div');
-        card.classList.add('card');
-        card.innerText = `Card ${i + 1}`;
-        card.addEventListener('click', () => selectCard(card, i));
+        let card = generateCard();
         document.getElementById('gameBoard').appendChild(card);
     }
 }
 
-function selectCard(cardElement, cardIndex) {
-    // Add or remove card from selection
-    if (selectedCards.includes(cardIndex)) {
-        selectedCards = selectedCards.filter(index => index !== cardIndex);
+function generateCard() {
+    // Generate random properties for each card
+    const number = getRandomInt(1, 3); // 1, 2, or 3 symbols
+    const symbol = ['diamond', 'squiggle', 'oval'][getRandomInt(0, 2)];
+    const shading = ['solid', 'striped', 'open'][getRandomInt(0, 2)];
+    const color = ['red', 'green', 'purple'][getRandomInt(0, 2)];
+
+    // Create card element
+    let card = document.createElement('div');
+    card.classList.add('card');
+    for (let n = 0; n < number; n++) {
+        let symbolElement = document.createElement('div');
+        symbolElement.classList.add('symbol', symbol, shading, color);
+        card.appendChild(symbolElement);
+    }
+    card.addEventListener('click', () => selectCard(card));
+
+    return card;
+}
+
+function selectCard(cardElement) {
+    // Simplified selection logic
+    if (cardElement.classList.contains('selected')) {
         cardElement.classList.remove('selected');
     } else {
-        selectedCards.push(cardIndex);
         cardElement.classList.add('selected');
     }
+    // Add logic here for checking sets
+}
 
-    // Check for set (simplified)
-    if (selectedCards.length === 3) {
-        document.getElementById('gameStatus').innerText = 'Set Selected!';
-        selectedCards = [];
-        document.querySelectorAll('.card').forEach(card => card.classList.remove('selected'));
-    }
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 newGame(); // Initialize game on load
